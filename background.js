ca7413aad4378566f8e63a5eb4114db9a2d5a921
Isaiah <gratwohl.isaiah@gmail.com>
2022-05-19 20:52:33 -0400

    Final version

--- background.js
+++ background.js
@@ -1,20 +1,61 @@
-chrome.runtime.onInstalled.addListener(async () => {
-  console.log("Extension installed");
+let requestingTabId;
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("NoQuora installed.");
 });
 
-/*chrome.tabs.onUpdated.addListener((id, info, tab) => {
-  console.log(tab.url.slice(0, 29));
-  if (tab.pendingUrl) {
-    console.log(tab.pendingUrl);
-    if (tab.pendingUrl.slice(0, 29) === "https://www.google.com/search") {
-      tab.pendingUrl = "https://pro-wash.ca";
-    }
-  }
-});*/
-
+//listen on new google search requests
 chrome.webRequest.onBeforeRequest.addListener(
   (details) => {
-    console.log(details);
+    //construct new search url
+    const searchFlag = "-inurl:quora.com";
+    const url = new URL(details.url);
+
+    const query = url.searchParams.get("q");
+    if (!query.includes(searchFlag)) {
+      url.searchParams.set("q", `${query} ${searchFlag}`);
+    }
+    //update tab id before next request
+    requestingTabId = details.tabId;
+    //return new request with modified search query
+    return { redirectUrl: url.toString() };
   },
-  { urls: ["https://*.google.com/*"] }
+  { urls: ["https://www.google.com/search?*"] },
+  ["blocking"]
+);
+
+//YzSd gsmt
+chrome.webRequest.onCompleted.addListener(
+  (details) => {
+    //script to be ran inside search window that hides the keyword
+    const script = () => {
+      function cleanSearchBox() {
+        const unescapedUrl = new URL(window.location.href);
+        if (unescapedUrl.searchParams.get("q").includes("-inurl:quora.com")) {
+          window.onload = () => {
+            const cleanString = (s) => {
+              return s.replaceAll(/(-[Ii]nurl:quora.com)/g, "").trim();
+            };
+
+            const searchBox = document.querySelector("[name='q']");
+            const businessPanel = document.querySelector("[class='YzSd gsmt']");
+
+            document.title = cleanString(document.title);
+            if (searchBox) {
+              searchBox.value = cleanString(searchBox.value);
+            }
+            if (businessPanel) {
+              businessPanel.innerHTML = cleanString(businessPanel.innerHTML);
+            }
+          };
+        }
+      }
+      cleanSearchBox();
+    };
+
+    //execute script
+    chrome.tabs.executeScript(requestingTabId, {
+      code: script.toString().slice(7, -1),
+    });
+  },
+  { urls: ["*://*.google.com/search?*"] }
 );
